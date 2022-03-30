231dff8e1bc80e9513d0465e4e8adc3ee468f01a
Claudiu C. Bogdan <claudiu.bogdan@devostack.com>
2022-03-30 18:27:20 +0300

    add: husky pre-commit hook

--- __tests__/index.test.jsx
+++ __tests__/index.test.jsx
@@ -1,12 +1,12 @@
-import { render, screen } from '@testing-library/react'
-import PosterCreator from '../pages/index'
+import { render, screen } from "@testing-library/react"
+import PosterCreator from "../pages/index"
 
-describe('Poster creator', () => {
-  it('renders a heading', () => {
+describe("Poster creator", () => {
+  it("renders a heading", () => {
     render(<PosterCreator />)
 
-    const heading = screen.getByRole('heading', {
-        name: /poster creator/i,
+    const heading = screen.getByRole("heading", {
+      name: /poster creator/i,
     })
 
     expect(heading).toBeInTheDocument()
