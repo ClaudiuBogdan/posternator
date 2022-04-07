664fcd41fe2a50b0a0a8d917dc26eb81b836153f
Claudiu C. Bogdan <claudiu.bogdan@devostack.com>
2022-04-07 08:49:25 +0300

    update: data format defined as map instead of array

--- components/molecules/PrintFormatPicker/data.ts
+++ components/molecules/PrintFormatPicker/data.ts
@@ -1,35 +1,34 @@
 import { PrintFormat } from "./types"
 
-export const printFormats: PrintFormat[] = [
-
-  {
+export const printFormats: {[key: string]: PrintFormat} = {
+  "A1": {
     id: "A1",
     name: "A1",
     width: 0.594,
     height: 0.841,
   },
-  {
+  "A2": {
     id: "A2",
     name: "A2",
     width: 0.420,
     height: 0.594,
   },
-  {
+  "A3": {
     id: "A3",
     name: "A3",
     width: 0.297,
     height: 0.420,
   },
-  {
+  "A4": {
     id: "A4",
     name: "A4",
     width: 0.210,
     height: 0.297,
   },
-  {
+  "A5": {
     id: "A5",
     name: "A5",
     width: 0.148,
     height: 0.210,
   },
-]
+}
--- components/molecules/PrintFormatPicker/index.tsx
+++ components/molecules/PrintFormatPicker/index.tsx
@@ -2,7 +2,7 @@ import { FC, useEffect, useState } from "react"
 import { RadioInput } from "components/atoms/RadioInput"
 import { printFormats } from "./data"
 import { PrintFormatSelectorStyled } from "./styles"
-import { PrintFormatId, PrintFormatSelectorProps as PrintFormatPickerProps } from "./types"
+import { PrintFormat, PrintFormatId, PrintFormatSelectorProps as PrintFormatPickerProps } from "./types"
 
 export const PrintFormatPicker: FC<PrintFormatPickerProps> = ({onChange}) => {
   const [selectedOptionId, setSelectedOption] = useState<PrintFormatId>("A4")
@@ -20,7 +20,7 @@ export const PrintFormatPicker: FC<PrintFormatPickerProps> = ({onChange}) => {
 
       <p>Paper print format: </p>
 
-      {printFormats.map((printFormat) => (
+      {Object.values(printFormats).map((printFormat) => (
         <RadioInput
           key={printFormat.id}
           id={printFormat.id}
@@ -32,3 +32,5 @@ export const PrintFormatPicker: FC<PrintFormatPickerProps> = ({onChange}) => {
     </PrintFormatSelectorStyled>
   )
 }
+
+export type {PrintFormat}
